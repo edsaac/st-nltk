4d07dde6880222280d0cc15ae22141aabf173acf
Ed Saa <esaavedrac@u.northwestern.edu>
2025-01-20 08:44:12 -0600

    Basic example

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import nltk
+
+_default_text = """Punkt knows that the periods in Mr. Smith and Johann S. 
+Bach do not mark sentence boundaries.  And sometimes sentences can start with 
+non-capitalized words.  i is a good variable name."""
+
+st.title("NKTK")
+text = st.text_area("Insert text to tokenize", value=_default_text)
+
+try:
+    sent_detector = nltk.tokenize.PunktTokenizer()
+
+except LookupError:
+    nltk.download("punkt_tab")
+    st.rerun()
+
+processed = sent_detector.tokenize(text.strip())
+
+st.header("Tokens:")
+for i, line in enumerate(processed, 1):
+    st.subheader(i, divider="blue")
+    st.write(line)
