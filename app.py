38f3d4de187eb80093457aa1790e2110c528d1ba
Ed Saa <esaavedrac@u.northwestern.edu>
2025-01-21 09:15:18 -0600

    Add page config

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 import streamlit as st
 import nltk
 
-_default_text = """Punkt knows that the periods in Mr. Smith and Johann S. 
-Bach do not mark sentence boundaries.  And sometimes sentences can start with 
-non-capitalized words.  i is a good variable name."""
 
-st.title("NLTK example")
-"Example from https://www.nltk.org/api/nltk.tokenize.punkt.html"
+def main():
+    st.set_page_config(
+        page_title="NLTK example in Streamlit · edsaac.me",
+        page_icon=":speech_balloon:",
+        menu_items={
+            "About": "https://edsaac.me",
+            "Report a Bug": "https://github.com/edsaac/st-nltk/issues",
+        },
+    )
 
-text = st.text_area("Insert text to tokenize", value=_default_text)
+    _default_text = """Punkt knows that the periods in Mr. Smith and Johann S. 
+    Bach do not mark sentence boundaries.  And sometimes sentences can start with 
+    non-capitalized words.  i is a good variable name."""
 
-try:
-    sent_detector = nltk.tokenize.PunktTokenizer()
+    st.title("NLTK in Streamlit")
+    "Example from https://www.nltk.org/api/nltk.tokenize.punkt.html"
 
-except LookupError:
-    nltk.download("punkt_tab")
-    st.rerun()
+    with st.expander("Make sure to download NLTK packages", expanded=True):
+        with st.echo():
+            try:
+                sent_detector = nltk.tokenize.PunktTokenizer()
 
-processed = sent_detector.tokenize(text.strip())
+            except LookupError:
+                nltk.download("punkt_tab")
+                st.rerun()
 
-st.header("Tokens:")
-for i, line in enumerate(processed, 1):
-    st.subheader(i, divider="blue")
-    st.write(line)
+    st.divider()
+    text = st.text_area("Insert text to tokenize", value=_default_text)
+    processed = sent_detector.tokenize(text.strip())
+
+    "**Tokens:**"
+    for i, line in enumerate(processed, 1):
+        st.write(f"**{i})** {line}")
+
+
+if __name__ == "__main__":
+    main()
